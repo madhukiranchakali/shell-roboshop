8144b1caaddfbbff55685f34f5da53a164e306a5
madhukiran chakali <madhukiranel@gmail.com>
2025-11-08 22:18:22 +0530

    some message

--- mongodb.sh
+++ mongodb.sh
@@ -39,3 +39,9 @@ VALIDATE $? "enable mongoDB"
 
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "start mongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarted MongoDB"
